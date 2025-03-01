d1fc7bf16db6b1b5b089d006d52fcf8e37a6e53d
Shaheer Arif <145375431+shaheer-m-arif@users.noreply.github.com>
2025-02-28 19:48:53 -0700

    Shaheer

--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include "config.h"
 #include "motor_control.h"
+#include "distance_sensor.h"
+#include "metal_detector.h"
+#include "remotecontroller.h"
+#include "wireless_remote.h"
+
+int main(void) {
+    distance_sensor_init();
+    metal_detector_init();
+    remotecontroller_init();
+    wireless_remote_init();
+
+    while (1) {
+        uint16_t distance = get_distance();
+        uint8_t metal = detect_metal();
+        uint8_t command = receive_command();
+        transmit_data("Hello");
+
+        // Do something with these values
+    }
+    return 0;
+}
